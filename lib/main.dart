b88e5416366bbb8fd20eea4b1a6637795dab0ae0
fanqilong <fanql@jxgtmbjt.com>
2020-02-18 18:05:09 +0800

    main 完成

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'dart:io';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_shop/provider/theme_provider.dart';
+import 'package:flutter_shop/routers/application.dart';
+import 'package:flutter_shop/routers/routers.dart';
 import 'package:flutter_shop/util/log_utils.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,11 +27,45 @@ class MyApp extends StatelessWidget {
   MyApp({this.home}) {
     Log.init();
     final router = Router();
-
+    Routes.configureRoutes(router);
+    Application.router = router;
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return OKToast(
+        child: ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => ThemeProvider(),
+          child: Consumer<ThemeProvider>(builder: (_, provider, __) {
+            return MaterialApp(
+              title: 'flutter shop',
+              theme: provider.getTheme(),
+              darkTheme: provider.getTheme(isDarkMode: true),
+              themeMode: provider.getThemeMode(),
+              onGenerateRoute: Application.router.generator,
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate
+              ],
+              supportedLocales: [Locale('zh', 'CH'), Locale('en', 'US')],
+              builder: (context, child) {
+                /// 保证文字大小不受手机系统设置影响 https://www.kikt.top/posts/flutter/layout/dynamic-text/
+                return MediaQuery(
+                  data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                  // 或者 MediaQueryData.fromWindow(WidgetsBinding.instance.window).copyWith(textScaleFactor: 1.0)
+                  child: child,
+                );
+              },
+            );
+          }),
+        ),
+
+        /// Toast 配置
+        backgroundColor: Colors.black54,
+        textPadding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+        radius: 20.0,
+        position: ToastPosition.bottom);
   }
 }
