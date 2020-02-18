0dd1160fa14e809eb977a452e875a9f249c63661
fanqilong <fanql@jxgtmbjt.com>
2020-02-18 17:46:59 +0800

    SplashPage 完成

---
+++ lib/home/splash_page.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+
+import 'package:flustars/flustars.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/common/common.dart';
+import 'package:flutter_shop/provider/theme_provider.dart';
+import 'package:flutter_shop/util/image_utils.dart';
+import 'package:flutter_shop/util/theme_utils.dart';
+import 'package:flutter_shop/widgets/load_image.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  int _status = 0;
+  List<String> _guideList = ['app_start_1', 'app_start_2', 'app_start_3'];
+  StreamSubscription _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await SpUtil.getInstance();
+      // 由于SpUtil初始化，所以MaterialApp获取的为默认主题配置，这里同步一下。
+      Provider.of<ThemeProvider>(context, listen: false).syncTheme();
+      if (SpUtil.getBool(Constant.keyGuide, defValue: true)) {
+        ///预先缓存图片 避免直接使用时因为首次加载造成闪动
+        _guideList.forEach((it) {
+          precacheImage(ImageUtils.getAssetImage(it), context);
+        });
+      }
+      _initSplash();
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  void _initSplash() {
+    _subscription =
+        Observable.just(1).delay(Duration(milliseconds: 1500)).listen((_) {
+      if (SpUtil.getBool(Constant.keyGuide, defValue: true)) {
+        SpUtil.putBool(Constant.keyGuide, false);
+        _initGuide();
+      } else {
+        _goLogin();
+      }
+    });
+  }
+
+  void _initGuide() {
+    setState(() {
+      _status = 1;
+    });
+  }
+
+  void _goLogin() {
+    //NavigatorUtils.push(context, LoginRouter.loginPage, replace: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: ThemeUtils.getBackgroundColor(context),
+        child: _status == 0
+            ? FractionallyAlignedSizedBox(
+                heightFactor: 0.3,
+                widthFactor: 0.33,
+                leftFactor: 0.33,
+                bottomFactor: 0,
+                child: LoadAssetImage('logo'))
+            : Swiper(
+                key: Key('swiper'),
+                itemCount: _guideList.length,
+                loop: false,
+                itemBuilder: (_, index) {
+                  return LoadAssetImage(_guideList[index],
+                      key: Key(_guideList[index]),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity);
+                },
+                onTap: (index) {
+                  if (index == _guideList.length - 1) {
+                    _goLogin();
+                  }
+                },
+              ));
+  }
+}
